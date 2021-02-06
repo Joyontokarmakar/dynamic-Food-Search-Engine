52da6d5cef9c7875c0d8cdbaae0d58e3e3c926e1
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-07 02:30:12 +0600

    food item show

--- js/theMealApi.js
+++ js/theMealApi.js
@@ -15,47 +15,24 @@ document.getElementById('submit').addEventListener("click", function(){
             mealDiv.className = 'col-4 my-2';
     
             const mealInfo = `
-                <a onclick="mealDetail('${meals.strMeal}')" href="" class="pt-2">
-                    <div class="single_meal p-3 text-center rounded shadow border child_height">
+                <div onclick="mealDetail('${meals.strMeal}')" class="single_meal p-3 text-center rounded shadow border child_height">
 
-                        <img src="${meals.strMealThumb}">
-                        <h4>${meals.strMeal}</h4>
-                        
-                    </div>
-                </a>
+                    <img src="${meals.strMealThumb}">
+                    <h4>${meals.strMeal}</h4>
+                    
+                </div>
             `
             mealDiv.innerHTML = mealInfo;
             mealsDiv.appendChild(mealDiv);
         })
+        
     }
 
 })
 
-    // const countryDetail = name =>{
-    //     const url = `https://restcountries.eu/rest/v2/name/${name}`
-    //     fetch(url)
-    //     .then(res => res.json())
-    //     .then(data => countryInfo(data[0]))
-    // }
-    
-    // const countryInfo = country => {
-    //     console.log(country)
-    //     const countryDiv = document.getElementById('country_information');
-    //     countryDiv.innerHTML = `
-    //         <img src="${country.flag}" class="border shadow-1">
-    //         <h4 class="text-center">${country.name}</h4>
-    //         <hr>
-    
-    //         <p><b>Native Name:</b> ${country.nativeName}</p>
-    //         <p><b>Capital:</b> ${country.capital}</p>
-    //         <p><b>Region:</b> ${country.region}</p>
-    //         <p><b>Sub-Region:</b> ${country.subregion}</p>
-    //         <p><b>Area:</b> ${country.area}</p>
-    //         <p><b>Population:</b> ${country.population}</p>
-    //         <p><b>Timezones:</b> ${country.timezones[0]}</p>
-    //         <p><b>Currency Name:</b> ${country.currencies[0].name}</p>
-    //         <p><b>Currency Code:</b> ${country.currencies[0].code}</p>
-    //         <p><b>Currency Symbol:</b> ${country.currencies[0].symbol}</p>
-    
-    //     `
-    // }
+const mealDetail = list =>{
+    const ingredientDiv = document.getElementById('meal_information');
+    ingredientDiv.innerHTML = `       
+        <h4>Ingredient List will show here</h4>
+    `
+}
