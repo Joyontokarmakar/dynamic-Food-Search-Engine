f9bc3a2cd03698cc31ec2c0a7fa0df0e6b71ca47
Joyonto Karmakar <41202398+Joyontokarmakar@users.noreply.github.com>
2021-02-07 11:12:01 +0600

    did something

--- js/theMealApi.js
+++ js/theMealApi.js
@@ -15,7 +15,7 @@ document.getElementById('submit').addEventListener("click", function(){
             mealDiv.className = 'col-4 my-2';
     
             const mealInfo = `
-                <div onclick="mealDetail('${meals.strMeal}')" class="single_meal p-3 text-center rounded shadow border child_height">
+                <div onclick="mealDetail('${meals.idMeal}')" class="single_meal p-3 text-center rounded shadow border child_height">
 
                     <img src="${meals.strMealThumb}">
                     <h4>${meals.strMeal}</h4>
@@ -32,33 +32,30 @@ document.getElementById('submit').addEventListener("click", function(){
 
 function mealDetail(mealName) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealName}`)
-        .then(res => res.json())
-        .then(data => {
-            const details = data.meals;
-            console.log(details)
-            // let htmlTemplateForDetails = '';
-            const mealDetails = document.getElementById('meal_information');
-            details.forEach(eachMeal => {
-            mealDetails.innerHTML = `
-                <div class="foodDetailsContainer">
-                    <img  src="${eachMeal.strMealThumb}" alt="food image">
-                    <h2>${eachMeal.strMeal}</h2>
-                    <h3>  Ingredients :</h3>
-                    
-                    <p><span>${eachMeal.strMeasure1}</span> <span>${eachMeal.strIngredient1}</span> </p>
-                    <p><span ${eachMeal.strMeasure2}</span> <span>${eachMeal.strIngredient2}</span> </p>
-                    <p><span>${eachMeal.strMeasure3}</span> <span>${eachMeal.strIngredient3}</span> </p>
-                    <p><span>${eachMeal.strMeasure4}</span> <span>${eachMeal.strIngredient4}</span> </p>
-                    <p><span>${eachMeal.strMeasure5}</span> <span>${eachMeal.strIngredient5}</span> </p>
-                    <p><span>${eachMeal.strMeasure6}</span> <span>${eachMeal.strIngredient6}</span> </p>
-                    <p><span>${eachMeal.strMeasure7}</span> <span>${eachMeal.strIngredient7}</span> </p>
-                    <p><span>${eachMeal.strMeasure8}</span> <span>${eachMeal.strIngredient8}</span> </p>
-                    <p><span>${eachMeal.strMeasure9}</span> <span>${eachMeal.strIngredient9}</span> </p>
-                    <p><span>${eachMeal.strMeasure10}</span> <span>${eachMeal.strIngredient10}</span> </p>
-                    <p><span>${eachMeal.strMeasure11}</span> <span>${eachMeal.strIngredient11}</span> </p>
-                    
-                </div>
-            `
+    .then(res => res.json())
+    .then(data => {
+        const details = data.meals;
+        console.log(details);
+
+        const mealDetails = document.getElementById('meal_information');
+        details.forEach(eachMeal => {
+        mealDetails.innerHTML = `
+            <img  src="${eachMeal.strMealThumb}" alt="food image">
+            <h4>${eachMeal.strMeal}</h4>
+            <hr>  
+            <p><b>Ingredients :</b></p>
+            <p><span>${eachMeal.strMeasure1}</span> <span>${eachMeal.strIngredient1}</span> </p>
+            <p><span ${eachMeal.strMeasure2}</span> <span>${eachMeal.strIngredient2}</span> </p>
+            <p><span>${eachMeal.strMeasure3}</span> <span>${eachMeal.strIngredient3}</span> </p>
+            <p><span>${eachMeal.strMeasure4}</span> <span>${eachMeal.strIngredient4}</span> </p>
+            <p><span>${eachMeal.strMeasure5}</span> <span>${eachMeal.strIngredient5}</span> </p>
+            <p><span>${eachMeal.strMeasure6}</span> <span>${eachMeal.strIngredient6}</span> </p>
+            <p><span>${eachMeal.strMeasure7}</span> <span>${eachMeal.strIngredient7}</span> </p>
+            <p><span>${eachMeal.strMeasure8}</span> <span>${eachMeal.strIngredient8}</span> </p>
+            <p><span>${eachMeal.strMeasure9}</span> <span>${eachMeal.strIngredient9}</span> </p>
+            <p><span>${eachMeal.strMeasure10}</span> <span>${eachMeal.strIngredient10}</span> </p>
+            <p><span>${eachMeal.strMeasure11}</span> <span>${eachMeal.strIngredient11}</span> </p>
+        `
         })
-    });
+    })
 }
